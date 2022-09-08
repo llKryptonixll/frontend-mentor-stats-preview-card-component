21cd35198177bda3b34d91775bfdfb81d18486aa
Lucas Cerri <lucascerri3@outlook.de>
2022-09-08 17:42:14 +0200

    project

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        
+        "main-background-color" : "var(--main-background-color)",
+        "card-background-color" : "var(--card-background-color)",
+        "accent-color": "var(--accent-color)",
+
+        "main-heading-color" : "var(--main-heading-color)",
+        "main-paragraph-color" : "var(--main-paragraph-color)",
+        "stat-heading-color" : "var(--stat-heading-color)"
+
+      },
+      fontFamily: {
+        "Inter": ["Inter", "sans-serif"]
+      },
+      backgroundImage: {
+        "image-header" : "url('/public/images/image-header-desktop.jpg')"
+      },
+      height: {
+        'custom': '1000px',
+        'small': '1300px'
+      },
+      brightness: {
+        40: '.40',
+      }
+    },
+  },
+  plugins: [],
+}
